0615ef1515ea4cd4d6e2eda902577bd22aa54392
JoelSuh07 <60123817+JoelSuh07@users.noreply.github.com>
2025-04-17 18:23:52 -0700

    Add files via upload

---
+++ homework3-problem3.cpp
@@ -0,0 +1,27 @@
+/*
+ * homework3-problem3.cpp
+ *
+ *  Created on: Apr 17, 2025
+ *      Author: Joel Suh
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int bmi(int m){
+	return
+}
+
+int main(){
+
+	cout << "Enter your weight in Kilograms: ";
+	cout << "Enter your height in Meters: ";
+
+
+
+}
+
+
+
+
