221cfeb9723cb90b5cc9caff5326b3a93f1b02c9
JoelSuh07 <joel.suh07@gmail.com>
2025-04-18 09:44:34 -0700

    commit

---
+++ homework4-problem2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+double a,b,c;
+
+bool isTriangle(double a, double b, double c){
+    if(a+b>c && a+c>b && b+c>a){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool isRightTriangle(double a, double b, double c){
+    if(a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a){
+        return true;
+    }else{
+        return false;
+    }
+}
+int main(){
+    cout << "Enter 3 digits: ";
+    cin >> a >> b >> c;
+    isTriangle(a,b,c) ? cout << "The numbers can form a triangle." 
+    : cout << "The numbers cannot form a triangle.";
+    isRightTriangle(a,b,c) ? cout << "The numbers can form a right triangle."
+    : cout << "The numbers cannot form a right triangle.";
+}
