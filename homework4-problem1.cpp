7fb026becb542db67ac8fe01b3b7dabf2baac825
JoelSuh07 <joel.suh07@gmail.com>
2025-04-18 12:48:04 -0700

    commit

---
+++ homework4-problem1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int digit,one,two,three,four,five;
+
+bool checkPalindrome(int digit){
+    one = digit / 10000;
+    two = (digit / 1000) % 10;
+    three = (digit / 100) % 10;
+    four = (digit / 10) % 10;
+    five = digit % 10;
+
+    if(one == five && two == four){
+        return true;
+} else{
+        return false;
+    }
+}
+
+int main(){
+    cout << "Enter a 5-digit number: ";
+    cin >> digit;
+    if(checkPalindrome(digit)){
+        cout << "The number is a palindrome." << endl;
+    }else{
+        cout << "The number is not a palindrome." << endl;
+    }
+
+return 0;
+}
