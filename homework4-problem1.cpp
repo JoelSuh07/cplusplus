3fcf9ef0c2ed3c62500c10b0f95abe403765b09f
JoelSuh07 <60123817+JoelSuh07@users.noreply.github.com>
2025-04-22 20:54:19 -0700

    Add files via upload

--- homework4-problem1.cpp
+++ homework4-problem1.cpp
@@ -1,31 +1,33 @@
-#include <iostream>
+/*
+ * homework4.cpp
+ *
+ *  Created on: Apr 22, 2025
+ *      Author: student
+ */
 
+#include <iostream>
 using namespace std;
 
-int digit,one,two,three,four,five,number;
-
-bool checkPalindrome(int digit){
-    one = digit / 10000;
-    two = (digit / 1000) % 10;
-    three = (digit / 100) % 10;
-    four = (digit / 10) % 10;
-    five = digit % 10;
-
-    if(one == five && two == four){
-        return true;
-} else{
-        return false;
-    }
-}
+double miles = 0;
+double gallons,totalMiles,totalGallons,MPG,totalMPG;
 
 int main(){
-    cout << "Enter a 5-digit number: ";
-    cin >> number;
-    if(checkPalindrome(number)){
-        cout << "The number is a palindrome." << endl;
-    }else{
-        cout << "The number is not a palindrome." << endl;
-    }
 
-return 0;
+	while(miles != -1){
+	cout << "Enter miles driven (-1 to quit): ";
+	cin >> miles;
+	cout << "Enter gallons used: ";
+	cin >> gallons;
+	MPG = miles/gallons;
+	cout << "MPG this trip: " << MPG << endl;
+	totalMiles+=miles;
+	totalGallons+=gallons;
+	totalMPG = totalMiles/totalGallons;
+	cout << "Total MPG: " << totalMPG << endl;
+	}
+
+	return 0;
 }
+
+
+
