19d96b4f6570515250433ce89c29bef3f0d9ed53
JoelSuh07 <60123817+JoelSuh07@users.noreply.github.com>
2025-04-22 21:23:45 -0700

    Add files via upload

---
+++ homework5-problem2.cpp
@@ -0,0 +1,32 @@
+/*
+ * homework5-problem2.cpp
+ *
+ *  Created on: Apr 22, 2025
+ *      Author: Joel Suh
+ *
+ *      An online retailer sells five products whose retail prices are as follows:
+ *      Product 1, $2.98;
+ *      product 2, $4.50;
+ *      product 3, $9.98;
+ *      product 4, $4.49 and
+ *      product 5, $6.87.
+ *      Write an application that reads a series of pairs of numbers as follows:
+ *      product number
+ *      quantity sold
+ *
+		Your program should use a switch statement to determine the retail price for each product.
+		It should calculate and display the total retail value of all products sold.
+		Use a sentinel-controlled loop to determine when the program should stop looping and display the final results.
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+}
+
+
+
+
