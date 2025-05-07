c7569b3b8d78d039975d6d801c5fc0a04fa378ca
JoelSuh07 <60123817+JoelSuh07@users.noreply.github.com>
2025-05-06 21:16:50 -0700

    Add files via upload

--- homework5/homework5-problem3.cpp
+++ homework5/homework5-problem3.cpp
@@ -2,9 +2,27 @@
  * homework5-problem3.cpp
  *
  *  Created on: May 6, 2025
- *      Author: student
+ *      Author: Joel Suh
  */
 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	for (int i=0;i<5;i++){
+		for (int k=4;k>0;k--){
+			cout << " ";
+		}
+		for (int j=0;j<10;j++){
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
 
 
 
