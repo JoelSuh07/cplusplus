0a27ae858773f1b487faf938fa02da29c776a53c
JoelSuh07 <joel.suh07@gmail.com>
2025-05-11 00:03:13 -0700

    Refactor pyramid drawing logic in homework5-problem3.cpp for correct output

--- homework5/homework5-problem3.cpp
+++ homework5/homework5-problem3.cpp
@@ -13,14 +13,22 @@ int i,k,j;
 
 int main(){
 
-	for (i=0;i<5;i++){
-		for (k=i;k<=5;k++){
+	for (i=1;i<=5;i++){
+		for (k=1;k<=5-i;k++){
 			cout << " ";
 		}
-		for (j=1;j<i+2;j++){
+		for (j=1;j<=2*i-1;j++){
 			cout << "*";
 		}
-
+		cout << endl;
+	}
+	for (i=4;i>0;i--){
+		for (k=1;k<=5-i;k++){
+			cout << " ";
+		}
+		for (j=1;j<=2*i-1;j++){
+			cout << "*";
+		}
 		cout << endl;
 	}
 
