f05c80fb9f5098bd58f14154d7e9a73f2e5645f4
JoelSuh07 <60123817+JoelSuh07@users.noreply.github.com>
2025-05-13 21:23:14 -0700

    Create homework6-problem2.cpp

---
+++ homework6/homework6-problem2.cpp
@@ -0,0 +1,26 @@
+/*
+ * homework6-problem2.cpp
+ *
+ *  Created on: May 13, 2025
+ *      Author: Joel Suh
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int num, MAX=1, MIN=1000;
+
+void isPerfect(int num);
+
+int main(){
+
+
+	return 0;
+}
+
+void isPerfect(int num){
+	for (num = MIN;num<=MAX;num++){
+		
+	}
+}
