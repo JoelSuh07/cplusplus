d349ab78a55e4ee164ad81fd71c3b4b42c860b9f
JoelSuh07 <60123817+JoelSuh07@users.noreply.github.com>
2025-05-20 21:18:37 -0700

    Create homework6-problem3.cpp

---
+++ homework6/homework6-problem3.cpp
@@ -0,0 +1,38 @@
+/*
+ * homework6-problem3.cpp
+ *
+ *  Created on: May 20, 2025
+ *      Author: student
+ */
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main(){
+
+	int x = rand();
+	int y = rand();
+	cout << "How much is " << x << " and " << y;
+
+
+}
+
+string goodJob(){
+	return
+	cout << "Very good!";
+	cout << "Excellent!";
+	cout << "Nice work!";
+	cout << "Keep up the good work!";
+}
+
+string badJob(){
+
+
+	return"Wrong. Try once more.";
+	return "No. Please try again.
+Wrong. Try once more.
+Don't give up!
+No. Keep trying.";
+}
