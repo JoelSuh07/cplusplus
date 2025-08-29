400bf18699297b97839ca24a82b3c336b96ea277
JoelSuh07 <60123817+JoelSuh07@users.noreply.github.com>
2025-08-28 19:49:22 -0700

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -2,20 +2,37 @@
  * lab.cpp
  *
  *  Created on: Aug 26, 2025
- *      Author: student
+ *      Author: Joel
  */
 #include <iostream>
 using namespace std;
 
 class Car{
 private:
+	int yearModel;
+	string make;
 	int speed;
-	double tank;
 
 public:
-	Car(){
+	Car(int YearModel, string Make){
 		cout << "Being called." << endl;
+		yearModel = YearModel;
+		make = Make;
 		speed = 0;
+	}
+
+	void setYearModel(int yM){
+		yearModel = yM;
+	}
+	int getYearModel(){
+		return yearModel;
+	}
+
+	void setMake(string m){
+		make = m;
+	}
+	string getMake(){
+		return make;
 	}
 
 	void setSpeed(int s){
@@ -24,35 +41,58 @@ public:
 	int getSpeed(){
 		return speed;
 	}
-
+/*
 	void setTank(double t){
 		tank =+ t;
 	}
 	double getTank(){
 		return tank;
 	}
-
+*/
 	void accelerate()
 	{
 		speed = speed + 5;
 	}
 
+	void brake(){
+		speed = speed - 5;
+	}
+
 	void display()
 	{
-		cout << "Speed = " << speed << endl;
+		cout << "Your " << yearModel << " " << make <<  "'s speed is " << speed << "." << endl;
 	}
 };
 
 int main(int argc, const char * argv[]){
 
-	Car car1;
+	Car car1(2009, "Toyota");
 
-	//car1.speed = 100;
+
 	car1.display();
+
 	car1.accelerate();
 	car1.display();
+		car1.accelerate();
+		car1.display();
+			car1.accelerate();
+			car1.display();
+				car1.accelerate();
+				car1.display();
+					car1.accelerate();
+					car1.display();
+
+	car1.brake();
+	car1.display();
+		car1.brake();
+		car1.display();
+			car1.brake();
+			car1.display();
+				car1.brake();
+				car1.display();
+					car1.brake();
+					car1.display();
 
 	return 0;
 }
-
 
